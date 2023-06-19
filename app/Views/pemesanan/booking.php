34001ff0419b9639407ab5eef23b3ff1dbc1c90e
ValentinusAdityo <136043378+ValentinusAdityo@users.noreply.github.com>
2023-06-19 16:35:36 +0700

    ubahBooking

--- app/Views/pemesanan/booking.php
+++ app/Views/pemesanan/booking.php
@@ -1,3 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+
+    <!-- ========== SEO ========== -->
+    <title><?= $title; ?></title>
+
+    <!-- ========== FAVICON ========== -->
+    <link rel="apple-touch-icon-precomposed" href="images/favicon-apple.png" />
+    <link rel="icon" href="images/favicon.png">
+
+    <!-- ========== STYLESHEETS ========== -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="revolution/css/layers.css" rel="stylesheet" type="text/css" />
+    <link href="revolution/css/settings.css" rel="stylesheet" type="text/css" />
+    <link href="revolution/css/navigation.css" rel="stylesheet" type="text/css" />
+    <link href="css/bootstrap-select.min.css" rel="stylesheet" type="text/css">
+    <link href="css/animate.min.css" rel="stylesheet" type="text/css">
+    <link href="css/famfamfam-flags.css" rel="stylesheet" type="text/css">
+    <link href="css/magnific-popup.css" rel="stylesheet" type="text/css">
+    <link href="css/owl.carousel.min.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/responsive.css" rel="stylesheet" type="text/css">
+
+    <!-- ========== ICON FONTS ========== -->
+    <link href="fonts/font-awesome.min.css" rel="stylesheet">
+    <link href="fonts/flaticon.css" rel="stylesheet">
+
+    <!-- ========== GOOGLE FONTS ========== -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900%7cRaleway:400,500,600,700" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
 <div id="smoothpage" class="wrapper">
 
     <!-- ========== HEADER ========== -->
@@ -219,3 +264,43 @@
             </div>
         </div>
     </main>
+
+    <!-- ========== FOOTER ========== -->
+<footer>
+    <div class="inner">
+    </div>
+</footer>
+</div>
+
+<!-- ========== BACK TO TOP ========== -->
+<div id="back_to_top">
+    <i class="fa fa-angle-up" aria-hidden="true"></i>
+</div>
+
+<!-- ========== NOTIFICATION ========== -->
+<div id="notification"></div>
+
+<!-- ========== JAVASCRIPT ========== -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/bootstrap-datepicker.min.js"></script>
+<script type="text/javascript" src="js/bootstrap-select.min.js"></script>
+<script type="text/javascript" src="js/jquery.smoothState.js"></script>
+<script type="text/javascript" src="js/moment.min.js"></script>
+<script type="text/javascript" src="js/morphext.min.js"></script>
+<script type="text/javascript" src="js/wow.min.js"></script>
+<script type="text/javascript" src="js/jquery.easing.min.js"></script>
+<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+<script type="text/javascript" src="js/owl.carousel.thumbs.min.js"></script>
+<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+<script type="text/javascript" src="js/jPushMenu.js"></script>
+<script type="text/javascript" src="js/isotope.pkgd.min.js"></script>
+<script type="text/javascript" src="js/imagesloaded.pkgd.min.js"></script>
+<script type="text/javascript" src="js/countUp.min.js"></script>
+<script type="text/javascript" src="js/jquery.countdown.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+
+</body>
+
+</html>
