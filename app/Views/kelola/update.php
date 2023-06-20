fb38badb7d10eb06e6a5b4945149a6b3a5ba307f
Anggarez <136096183+Anggarez@users.noreply.github.com>
2023-06-20 15:11:27 +0700

    Update update.php

--- app/Views/kelola/update.php
+++ app/Views/kelola/update.php
@@ -65,9 +65,9 @@
                 <aside class="widget">
                     <div class="vbf">
                         <h2 class="form_title"><i class="fa"></i>Update Kamar</h2>
-                        <form method="post" action="/input" class="inner" enctype="multipart/form-data">
+                        <form method="post" action="/update" class="inner" enctype="multipart/form-data">
                             <?= csrf_field() ?>
-                            
+
                             <div class="form-group">
                                 <input class="form-control" name="idKamar" placeholder="Masukkan ID Kamar" type="text">
                             </div>
