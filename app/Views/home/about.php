bd48cefd7761008f8de6f479676539c1db7a8dfa
gerimuti <136038547+gerimuti@users.noreply.github.com>
2023-06-21 22:08:05 +0700

    hehe

---
+++ app/Views/home/about.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+
+    <!-- ========== SEO ========== -->
+    <title>EUFORIAHOME</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    <meta content="" name="author">
+
+    <!-- ========== FAVICON ========== -->
+    <link rel="apple-touch-icon-precomposed" href="images/favicon-apple.png" />
+    <link rel="icon" href="images/favicon.png">
+
+    <!-- ========== STYLESHEETS ========== -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="revolution/css/layers.css" rel="stylesheet" type="text/css" />
+    <link href="revolution/css/settings.css" rel="stylesheet" type="text/css" />
+    <link href="revolution/css/navigation.css" rel="stylesheet" type="text/css" />
+    <link href="css/bootstrap-select.min.css" rel="stylesheet" type="text/css">
+    <link href="css/animate.min.css" rel="stylesheet" type="text/css">
+    <link href="css/famfamfam-flags.css" rel="stylesheet" type="text/css">
+    <link href="css/magnific-popup.css" rel="stylesheet" type="text/css">
+    <link href="css/owl.carousel.min.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/responsive.css" rel="stylesheet" type="text/css">
+
+    <!-- ========== ICON FONTS ========== -->
+    <link href="fonts/font-awesome.min.css" rel="stylesheet">
+    <link href="fonts/flaticon.css" rel="stylesheet">
+
+    <!-- ========== GOOGLE FONTS ========== -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900%7cRaleway:400,500,600,700" rel="stylesheet">
+</head>
+<!-- ========== HEADER ========== -->
+<header class="fixed">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle mobile_menu_btn" data-toggle="collapse" data-target=".mobile_menu" aria-expanded="false">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html">
+                <img src="images/logo.svg" height="32" alt="Logo">
+            </a>
+        </div>
+        <nav id="main_menu" class="mobile_menu navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="mobile_menu_title" style="display:none;">MENU</li>
+                <li class="dropdown simple_menu">
+                    <a href="#" class="" data-toggle="dropdown">HOME <b class="caret"></b></a>
+                </li>
+                <li class="dropdown simple_menu">
+                    <a href="#" class="" data-toggle="dropdown">ROOMS <b class="caret"></b></a>
+                </li>
+                <li class="dropdown mega_menu mega_menu_fullwidth"><a href="#" data-toggle="dropdown" class="" aria-expanded="true">ABOUT<b class="caret"></b></a>
+                </li>
+                </li>
+                <li><a href="contact.html">CONTACT US</a></li>
+                <li class="active"><a href="blog.html">SIGN IN</a></li>
+                <li class="menu_button">
+                    <a href="booking-form.html" class="button  btn_yellow"><i class="fa fa-calendar"></i>REGISTER</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<!-- =========== PAGE TITLE ========== -->
+<div class="page_title gradient_overlay" style="background: url(images/page_title_bg.jpg);">
+    <div class="container">
+        <div class="inner">
+            <h1>Tentang Kami</h1>
+            <ol class="breadcrumb">
+                <li><a href="index.html">Home</a></li>
+                <li>Tentang Kami</li>
+            </ol>
+        </div>
+    </div>
+</div>
+
+<!-- =========== MAIN ========== -->
+<main class="blog">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <!-- ITEM -->
+                <article class="blog_list">
+                    <figure>
+                        <a href="blog-post.html" class="hover_effect h_link h_blue">
+                            <img src="images/tampilan kost.jpeg" class="img-responsive" alt="Image">
+                        </a>
+                    </figure>
+                    <div class="details">
+                        <h2><a href="blog-post.html">Tentang Kami </a></h2>
+                        <div class="info">
+                            <span class="meta_part"><a href="#"><i class="fa fa-user"></i>Teddy </a></span>
+                            <span class="meta_part"><a href="#"><i class="fa fa-calendar"></i>February 15, 2017</a></span>
+                            <span class="meta_part"><a href="#"><i class="fa fa-comment-o"></i>68 Comments</a></span>
+                            <span class="meta_part"><i class="fa fa-folder-open-o"></i><a href="#">News</a>, <a href="#">Events</a></span>
+                        </div>
+                        <p>Selamat datang di EuforiaHome!
+
+                            Kami di EuforiaHome berkomitmen untuk menciptakan pengalaman hunian yang tak terlupakan bagi setiap pelanggan kami. Sebagai perusahaan desain interior dan kontraktor rumah yang inovatif, kami memahami betapa pentingnya rumah yang nyaman dan estetis bagi kehidupan sehari-hari Anda.
+                        </p>
+                        EuforiaHome didirikan dengan tujuan untuk mengubah rumah biasa menjadi ruang yang menginspirasi, memadukan gaya dan fungsi dengan harmonis. Dengan keahlian kami dalam desain interior dan pengetahuan mendalam tentang industri konstruksi, kami siap membantu Anda mewujudkan visi hunian impian Anda.
+                        </p>
+                        Tim kami terdiri dari para ahli desain interior yang berbakat dan berpengalaman serta tukang yang terampil dan berdedikasi. Kami percaya bahwa kolaborasi yang erat dengan pelanggan adalah kunci kesuksesan. Kami akan mendengarkan kebutuhan, preferensi, dan inspirasi Anda dengan seksama untuk menciptakan desain yang sesuai dengan kepribadian dan gaya hidup Anda.
+                        </p>
+                        Kami menawarkan berbagai layanan yang mencakup desain interior, renovasi, dan konstruksi rumah. Dengan bahan-bahan berkualitas tinggi dan pemahaman yang mendalam tentang tren terkini dalam desain, kami akan menghadirkan sentuhan kreatif dan solusi fungsional ke dalam setiap ruang di rumah Anda.
+                        </p>
+                        Kepuasan pelanggan adalah prioritas utama kami. Kami berkomitmen untuk memberikan layanan yang personal, transparan, dan mengutamakan kualitas. Setiap proyek kami dijalankan dengan ketelitian dan dedikasi yang tinggi, untuk memastikan bahwa hasil akhirnya melebihi harapan Anda.
+                        </p>
+                        Apapun kebutuhan desain dan konstruksi Anda, EuforiaHome siap menjadi mitra terpercaya Anda. Kami berdedikasi untuk memberikan pengalaman yang tak terlupakan dan menciptakan ruang yang memenuhi hasrat estetika Anda. Mari bersama-sama mewujudkan hunian yang indah dan menginspirasi!
+                        </p>
+                        Terima kasih telah memilih EuforiaHome.
+                        </p>
+                        Salam,
+                        </p>
+                        Tim EuforiaHome</p>
+                        <a class="button btn_blue " href="blog-post.html"><i class="fa fa-angle-double-right"></i> Read More </a>
+                    </div>
+                </article>
+            </div>
+        </div>
+    </div>
+    </div>
+
+</main>
+
+
+<!-- ========== BACK TO TOP ========== -->
+<div id="back_to_top">
+    <i class="fa fa-angle-up" aria-hidden="true"></i>
+</div>
+
+<!-- ========== JAVASCRIPT ========== -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/bootstrap-datepicker.min.js"></script>
+<script type="text/javascript" src="js/bootstrap-select.min.js"></script>
+<script type="text/javascript" src="js/jquery.smoothState.js"></script>
+<script type="text/javascript" src="js/moment.min.js"></script>
+<script type="text/javascript" src="js/morphext.min.js"></script>
+<script type="text/javascript" src="js/wow.min.js"></script>
+<script type="text/javascript" src="js/jquery.easing.min.js"></script>
+<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+<script type="text/javascript" src="js/owl.carousel.thumbs.min.js"></script>
+<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+<script type="text/javascript" src="js/jPushMenu.js"></script>
+<script type="text/javascript" src="js/isotope.pkgd.min.js"></script>
+<script type="text/javascript" src="js/imagesloaded.pkgd.min.js"></script>
+<script type="text/javascript" src="js/countUp.min.js"></script>
+<script type="text/javascript" src="js/jquery.countdown.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+
+</body>
+
+</html>
