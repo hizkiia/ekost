4032d4efc772aa03d0f496d79bdb99a39339631e
hizkiia <109422084+hizkiia@users.noreply.github.com>
2023-10-06 11:18:54 +0700

    register

--- app/Controllers/RegisterController.php
+++ app/Controllers/RegisterController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Models\PelangganModel;
+
 
 
 class RegisterController extends BaseController
@@ -12,6 +14,37 @@ class RegisterController extends BaseController
         $data = ['title' => 'EuforiaHome|Register'];
         return view('layout/header', $data)
             . view('layout/navbarGuest') . view('register/register') . view('layout/footer');
+    }
+
+    public function input()
+    {
+        $model = model(PelangganModel::class);
+        $data = [
+            'list' => $model->getPelanggan(),
+            'title' => 'Register'
+        ];
+
+        // Memeriksa apakah melakukan submit data atau tidak.
+        if (!$this->request->is('post')) {
+            return view('layout/header', $data) . view('layout/navbarGuest')
+                . view('register/register')
+                . view("layout/footer");
+        }
+
+
+        // Mengambil data yang disubmit dari form
+        $post = $this->request->getPost([
+            'email',
+            'username',
+            'name',
+            'phone',
+            'address',
+            'password'
+        ]);
+
+        // Mengakses Model untuk menyimpan data
+        $model->simpan($post);
+        return redirect()->to('/login');
     }
 
 
--- app/Models/PelangganModel.php
+++ app/Models/PelangganModel.php
@@ -7,5 +7,26 @@ use CodeIgniter\Model;
 class PelangganModel extends Model
 {
     protected $table = 'pelanggan';
-    protected $allowedFields = ['pelanggan_id', 'nama', 'alamat', 'no_hp', 'password'];
+    protected $allowedFields = ['pelanggan_id', 'nama', 'alamat', 'no_hp', 'password', 'email'];
+
+    public function getPelanggan($slug = false)
+    {
+        if ($slug === false) {
+            return $this->findAll();
+        }
+
+        return $this->where(['pelanggan_id' => $slug])->first();
+    }
+
+    public function simpan($record)
+    {
+        $this->save([
+            'pelanggan_id' => $record['username'],
+            'nama' => $record['name'],
+            'alamat' => $record['address'],
+            'no_hp' => $record['phone'],
+            'password' => $record['password'],
+            'email' => $record['email'],
+        ]);
+    }
 }
